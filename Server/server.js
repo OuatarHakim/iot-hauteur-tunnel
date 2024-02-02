aeac41479f361fd48d2a2099ed7d0fb4969cb2fb
Ouatar Hakim <hakim.ouatar6@gmail.com>
2024-02-02 14:09:58 +0100

    creation de docker et server

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,10 @@
-// server.js
 const express = require('express');
 const http = require('http');
-const socketIO = require('socket.io');
+const WebSocket = require('ws');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server);
+const wss = new WebSocket.Server({ server });
 
 const port = 3000;
 
@@ -13,17 +12,22 @@ app.get('/', (req, res) => {
   res.send('Serveur IoT');
 });
 
-io.on('connection', (socket) => {
+wss.on('connection', (ws) => {
   console.log('Un client s\'est connecté');
 
-  socket.on('disconnect', () => {
+  ws.on('close', () => {
     console.log('Client déconnecté');
   });
 
-  socket.on('vehicleHeight', (height) => {
-    if (height > limiteHauteur) {
-      // Si la hauteur dépasse la limite, on envoie un message au client
-      io.emit('message', 'Attention! La hauteur du véhicule est trop grande.');
+  ws.on('message', (message) => {
+    const { type, data } = JSON.parse(message);
+
+    if (type === 'vehicleHeight') {
+      const height = parseFloat(data);
+      if (!isNaN(height) && height > limiteHauteur) {
+        // Si la hauteur dépasse la limite, on envoie un message au client
+        ws.send(JSON.stringify({ type: 'message', data: 'Attention! La hauteur du véhicule est trop grande.' }));
+      }
     }
   });
 });

82c967a9484de68d82eecaabcf03a1e26cc07556
Ouatar Hakim <hakim.ouatar6@gmail.com>
2024-02-02 12:53:07 +0100

    initialisation de serveur afin de recevoir des distance et envoie des messages

--- Server/server.js
+++ Server/server.js
@@ -1,22 +1,35 @@
 // server.js
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Serveur IoT');
 });
 
-app.post('/distance', (req, res) => {
-  // Recevoir la distance depuis Arduino et prendre des actions en conséquence
-  const distance = req.body.distance;
+io.on('connection', (socket) => {
+  console.log('Un client s\'est connecté');
 
-  // Logique pour demander de ne pas avancer si la hauteur du véhicule est trop grande
+  socket.on('disconnect', () => {
+    console.log('Client déconnecté');
+  });
 
-  res.send(`Distance reçue : ${distance}`);
+  socket.on('vehicleHeight', (height) => {
+    if (height > limiteHauteur) {
+      // Si la hauteur dépasse la limite, on envoie un message au client
+      io.emit('message', 'Attention! La hauteur du véhicule est trop grande.');
+    }
+  });
 });
 
-app.listen(port, () => {
+const limiteHauteur = 200;  // la limite 
+
+server.listen(port, () => {
   console.log(`Serveur en écoute sur le port ${port}`);
 });
-

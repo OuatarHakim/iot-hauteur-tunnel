0b7a9f51c93958e889a34894ccc0f5d28c668067
Mourad <mourad.hadj-ali@etu.univ-lehavre.fr>
2024-02-03 16:21:56 +0100

    Mise à jour de l'interface HTML pour afficher la distance et le statut du véhicule provenant des données Arduino.

--- Server/server.js
+++ Server/server.js
@@ -1,44 +1,58 @@
-
-
-const { SerialPort } = require('serialport');
-const express = require('express');
-const http = require('http');
-const Readline = require('@serialport/parser-readline');
+const { SerialPort } = require("serialport");
+const express = require("express");
+const http = require("http");
+const Readline = require("@serialport/parser-readline");
 
 const app = express();
 const server = http.createServer(app);
 
 const port = 3000;
 
-const serialPort = new SerialPort({ 
-  path:'/dev/ttyACM0',
+const serialPort = new SerialPort({
+  path: "/dev/ttyACM0",
   baudRate: 115200,
   dataBits: 8,
-  parity: 'none',
+  parity: "none",
   stopBits: 1,
   flowControl: false,
 });
 
-const parser = serialPort.pipe(new Readline.ReadlineParser({ delimiter: '\r\n' }));
+const parser = serialPort.pipe(
+  new Readline.ReadlineParser({ delimiter: "\r\n" })
+);
 
-serialPort.on('open', function() {
-  console.log('Port série ouvert');
+serialPort.on("open", function () {
+  console.log("Port série ouvert");
 });
 
-serialPort.on('error', function(err) {
-  console.error('Erreur :', err.message);
+serialPort.on("error", function (err) {
+  console.error("Erreur :", err.message);
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
-
-app.get('/data', (req, res) => {
-  parser.once('data', (data) => {
-    res.json({ type: 'arduinoData', data: data.trim() });
+/*
+app.get("/data", (req, res) => {
+  parser.once("data", (data) => {
+    res.json({ type: "arduinoData", data: data.trim() });
   });
 });
+*/
+app.get("/data", (req, res) => {
+  parser.once("data", (data) => {
+    const distance = parseFloat(data.trim());
+    const isAuthorized = distance >= 100;
 
+    const response = {
+      type: "vehicleStatus",
+      distance: distance,
+      data: isAuthorized ? "Autorisé" : "Non Autorisé",
+    };
+
+    res.json(response);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Serveur en écoute sur le port ${port}`);

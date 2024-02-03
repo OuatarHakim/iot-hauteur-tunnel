2fcc81b710419ac90c8d645b8e4dc9600d778128
Mourad <mourad.hadj-ali@etu.univ-lehavre.fr>
2024-02-03 16:22:50 +0100

    mise a jour de la logique arduino

--- Arduino/distance.ino
+++ Arduino/distance.ino
@@ -4,7 +4,7 @@ const byte TRIGGER = 6;                        // Broche TRIGGER
 const byte ECHO = 7;                           // Broche ECHO
 const float SOUND_SPEED = 340.0 / 1000;        // Vitesse du son dans l'air en mm/us
 const unsigned long MEASURE_TIMEOUT = 25000UL; // 25ms = 8m à 340m/s
-const float MAX_ALLOWED_HEIGHT = 200.0;        // Hauteur maximale autorisée en mm
+const float MAX_ALLOWED_HEIGHT = 100.0;        // Hauteur maximale autorisée en mm
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup()
@@ -41,32 +41,25 @@ void loop()
 
     lcd.display();
 
-    if (distance_mm > 600)
+    // Vérifier la hauteur du véhicule et allumer la LED appropriée
+    if (distance_mm == 0)
     {
+
+        digitalWrite(8, LOW); // LED rouge éteinte
+        digitalWrite(9, LOW); // LED verte éteinte
+    }
+    else if (distance_mm <= MAX_ALLOWED_HEIGHT)
+    {
+        // La hauteur du véhicule n'est pas autorisée
         digitalWrite(8, HIGH); // LED rouge allumée
         digitalWrite(9, LOW);  // LED verte éteinte
     }
-    else if (distance_mm < 1)
-    {
-        digitalWrite(8, LOW);  // LED rouge éteinte
-        digitalWrite(9, HIGH); // LED verte allumée
-    }
     else
     {
-        // Vérifier la hauteur du véhicule et allumer la LED appropriée
-        if (distance_mm <= MAX_ALLOWED_HEIGHT)
-        {
-            // La hauteur du véhicule n'est pas autorisée
-            digitalWrite(8, HIGH); // LED rouge allumée
-            digitalWrite(9, LOW);  // LED verte éteinte
-        }
-        else
-        {
 
-            // La hauteur du véhicule est autorisée
-            digitalWrite(8, LOW);  // LED rouge éteinte
-            digitalWrite(9, HIGH); // LED verte allumée
-        }
+        // La hauteur du véhicule est autorisée
+        digitalWrite(8, LOW);  // LED rouge éteinte
+        digitalWrite(9, HIGH); // LED verte allumée
     }
 
     // Ajouter un court délai pour éviter d'envoyer trop fréquemment

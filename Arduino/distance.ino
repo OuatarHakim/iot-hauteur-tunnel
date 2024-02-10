fbf2855cc4bea2f4a4dc2bf827f4b5e8f5991719
Ouatar Hakim <hakim.ouatar6@gmail.com>
2024-02-10 17:00:05 +0100

    récupération de la distance limite envoyé par le serveur et revoie des hauteurs inférieur a la distance limite

--- Arduino/distance.ino
+++ Arduino/distance.ino
@@ -4,7 +4,7 @@ const byte TRIGGER = 6;                        // Broche TRIGGER
 const byte ECHO = 7;                           // Broche ECHO
 const float SOUND_SPEED = 340.0 / 1000;        // Vitesse du son dans l'air en mm/us
 const unsigned long MEASURE_TIMEOUT = 25000UL; // 25ms = 8m à 340m/s
-const float MAX_ALLOWED_HEIGHT = 100.0;        // Hauteur maximale autorisée en mm
+ float MAX_ALLOWED_HEIGHT ;        // Hauteur maximale autorisée en mm
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup()
@@ -22,6 +22,13 @@ void setup()
 
 void loop()
 {
+    if (Serial.available() > 0)
+    {
+        String data = Serial.readStringUntil('\n');
+        MAX_ALLOWED_HEIGHT = data.toFloat();
+        Serial.print("Distance maximale autorisée mise à jour: ");
+        Serial.println(MAX_ALLOWED_HEIGHT);
+    }
     digitalWrite(TRIGGER, HIGH);
     delayMicroseconds(10);
     digitalWrite(TRIGGER, LOW);
@@ -30,10 +37,13 @@ void loop()
     float distance_mm = measure / 2.0 * SOUND_SPEED;
 
     // Envoyer la distance et MAX_ALLOWED_HEIGHT via le port série
-    Serial.print(distance_mm);
-    Serial.print(",");
-    Serial.println(MAX_ALLOWED_HEIGHT);
+    if (distance_mm <= MAX_ALLOWED_HEIGHT)
+    {
 
+        Serial.print(distance_mm);
+        Serial.print(",");
+        Serial.println(MAX_ALLOWED_HEIGHT);
+    }
     lcd.print(distance_mm / 10.0, 2);
     lcd.print("  CM");
     lcd.display();
